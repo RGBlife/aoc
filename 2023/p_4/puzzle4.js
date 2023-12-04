d248521d4b9c2ff1da883edeff856eb8a70808c8
Bret Gomes <47285194+RGBlife@users.noreply.github.com>
2023-12-04 21:56:03 +0000

    puzzle 4 completed on the iPad

--- 2023/p_4/puzzle4.js
+++ 2023/p_4/puzzle4.js
@@ -1,19 +1,75 @@
-import { readTxtFile } from "../../utils/readTxtFile.mjs";const allocatedCubes = {
-  red: 12,
-  green: 13,
-  blue: 14,
-};
+import { readTxtFile } from "../../utils/readTxtFile.mjs";
 
-async function checkPossibleGames() {
-  const rawTxtList = await readTxtFile("../2023/p_3/inputs.txt");
-  const listOfGames = getListOfGames(rawTxtList);
-
-  const possibleGamesIds = [];
-
-  listOfGames.forEach((game) => {
+async function checkFewestCubes() {
+  try {
+    const rawTxtList = await readTxtFile("../2023/p_3/inputs.txt")
+    const listOfGames = getListOfGames(rawTxtList);
+    
+    const powerCol = []
+    
+    
+    
+    listOfGames.forEach((game) => {
+      const playedCubes = {
+      red: 0,
+  green: 0,
+  blue: 0,
+    }
+      
     Object.entries(game).forEach(([gameId, plays]) => {
-      if (!isGameImpossible(plays)) {
-        possibleGamesIds.push(gameId);
-      }
+      
+      
+      
+      plays.forEach((play)=>{
+        const cubes = play.split(", ");
+        cubes.forEach((cube)=>{
+          
+          const [number, colour] = cube.split(" ");
+          
+          playedCubes[colour] = Math.max(number, playedCubes[colour])
+          
+        })
+      })
+      
+      
+     
+     // console.log(plays)
+      
     });
+    let multiply = 1
+    for (const key in playedCubes) {
+      multiply *= playedCubes[key]
+    }
+    powerCol.push(multiply)
+    //console.log(multiply)
+    //game
   });
+  
+  const sum = powerCol.reduce((acc, cur) => {
+    return acc + parseInt(cur);
+  }, 0);
+  
+  return sum
+    
+  } catch (err){
+    console.log(err)
+  }
+  
+}
+
+function getListOfGames(txtList) {
+  const listOfGames = txtList.map((game) => {
+    const gameSplit = game.split(":");
+    const key = gameSplit[0].substring(5).trim();
+    const value = gameSplit[1].trim().split("; ");
+
+    const obj = {};
+    obj[key] = value;
+    return obj;
+  });
+  return listOfGames;
+}
+
+
+  
+checkFewestCubes().then((res)=>{console.log(res)})
