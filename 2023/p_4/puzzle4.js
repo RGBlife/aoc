9acca7cfe4d077960ed6620b8f14ad014f46462b
Bret Gomes <47285194+RGBlife@users.noreply.github.com>
2023-12-04 20:45:33 +0000

    test

--- 2023/p_4/puzzle4.js
+++ 2023/p_4/puzzle4.js
@@ -1,1 +1,19 @@
-import { readTxtFile } from "../../utils/readTxtFile.mjs";
+import { readTxtFile } from "../../utils/readTxtFile.mjs";const allocatedCubes = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+async function checkPossibleGames() {
+  const rawTxtList = await readTxtFile("../2023/p_3/inputs.txt");
+  const listOfGames = getListOfGames(rawTxtList);
+
+  const possibleGamesIds = [];
+
+  listOfGames.forEach((game) => {
+    Object.entries(game).forEach(([gameId, plays]) => {
+      if (!isGameImpossible(plays)) {
+        possibleGamesIds.push(gameId);
+      }
+    });
+  });
